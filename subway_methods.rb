43497221aa0556ce7de603a3f79d42ecce23d1bb
Nicole Bassolino <nbassolino@thacher.org>
2016-06-21 18:09:57 +0000

    I finished the Subway Method Lab

--- subway_methods.rb
+++ subway_methods.rb
@@ -5,27 +5,39 @@ def map_size
   "The map is #{21 * 25} square inches"
 end
 
-# Now you give it a shot! It takes about 156 seconds to go between stops and you'll be taking the train for 3 stops. Write a method that returns how long your trip will take. 
+# Now you give it a shot! It takes about 156 seconds to go between stops and you'll be taking the train for 3 stops. Write a method that returns how long your trip will take.
 def trip_length
-	# write your code here
+	"The trip will take #{156 * 3} seconds."
 end
 
 # While you are waiting for the train you decide to play a little dice. Create a method called roll that returns a random number between 1 and 6 (feel free to google "how to generate a random number in ruby").
 
-# write your code here
+def roll
+  1 + rand(6)
+end
 
-
-
-# The train arrives and you hop on. Guess what time it is? It's showtime! There are 23 people on the train and each person gives the dancers 1.5 dollars. Write a method called pay_dancers that returns how much money they made. 
-
+# The train arrives and you hop on. Guess what time it is? It's showtime! There are 23 people on the train and each person gives the dancers 1.5 dollars. Write a method called pay_dancers that returns how much money they made.
+def pay_dancers
+  "The dancers were paid #{23 * 1.5} dollars."
+end
 
 # There is one grumpy lady on the train that doesn't like the dancing though. Write a method called stop_dancing that returns a message to the dancers in all caps.
-
+ def stop_dancing
+  "stop dancing".upcase
+end
 
 # There is also a really enthusiastic rider who keeps shouting "Everything is awesome!" Write a method called everything_is_awesome that returns one string with "Everything is awesome!" 5 times. (Ruby has a `.times` method that can help you do this. Think about setting up an empty string and adding to it five times)
+def everything_is_awesome
+  5.times do
+    puts "Everything is awesome!"
+  end
+end
 
 
 # You are almost at your stop and you start thinking about how you are going to get home. You have $18 dollars left on your metro card (For those of you not in New York, the price of a metro card is now $2.75). Write a method called remaining_trips that return how many trips you have left.
+def remaining_trips
+  "#{18/2.75}"
+end
 
 
 
@@ -38,16 +50,16 @@ puts "This is how long the trip will take"
 puts trip_length
 
 puts "While I was waiting I rolled a"
-# call your method here
+puts roll
 
 puts "How much money did the train dancers make?"
-# call your method here
+puts pay_dancers
 
 puts "That lady told the train dancers to"
-# call your method here
+puts stop_dancing
 
 puts "That guy kept shouting"
-# call your method here
+everything_is_awesome
 
 puts "This is how many trips I have left on my metrocard"
-# call your method here
+puts remaining_trips
